89c5f6c2a14ba03330532719d173e336cc726642
windyimp81 <windyimp@spesys.co.kr>
2023-02-13 19:12:07 +0900

    3 mode

--- src/App2.tsx
+++ src/App2.tsx
@@ -1,10 +1,12 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
+import useInterval from "./hooks/useInterval";
 import './App.css';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement,
   LineElement, Title, Tooltip, Legend } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import { Card, useTheme, styled, Box, Grid, Button } from '@mui/material';
 import axios from 'axios';
+import ChartHumi from './component/ChartHumi';
 
 ChartJS.register(
   CategoryScale,
@@ -18,38 +20,39 @@ ChartJS.register(
 
 function App() {
 
-  const [data1, setData1] = useState({
-    sensor_value: 0,
-    receive_time: ''
-  });
+  const mountedDust100 = useRef(false);
+  const [sensorValueDust100, setSensorValueDust100] = useState<number[]>([]);
+  const [receiveTimeDust100, setReceiveTimeDust100] = useState<string[]>([]);
 
-  useEffect(() => {
-    setInterval(
-      () => axios.get(`http://43.201.154.68:8100/Get_Value?sensor_type=DUST100`)
-      .then((response) => {
-        setData1(response.data['data'][0]);
-      }), 2000
-    )
-  }, []);
-  const [sensorValue, setSensorValue] = useState(0);
-  const [receiveTime, setReceiveTime] = useState('');
-  // const fetchDatas = async() => {
-  //     const response = await axios.get('http://43.201.154.68:8100/Get_Value?sensor_type=DUST100');
-  //     setData1(response.data['data'][0]);
-  // };
+  const [isStartDust100, setIsStartDust100] = useState(true);
 
-  // useEffect(() => {
-  //     fetchDatas();
-  // }, []);
+  function toggleStartDust100(){
+    setIsStartDust100(!isStartDust100);
+    mountedDust100.current=isStartDust100
+  }
 
-  useEffect(() => {
-      let sv = Number(data1?.sensor_value);
-      let st = data1.receive_time;
-      setSensorValue(sv);
-      setReceiveTime(st);
-  }, [data1])
+  useEffect(()=> {
+    console.log("effect works", mountedDust100.current);
+    if(!mountedDust100.current) {
+      mountedDust100.current = true;
+    }
+    else {
+      const loop = setInterval(
+        () => axios.get(`http://43.201.154.68:8100/Get_Value?sensor_type=DUST100`)
+        .then((response) => {
+          console.log("res");
+          const da = response.data['data'][0];
+          let sv = Number(da.sensor_value);
+          let st = da.receive_time;
+          setSensorValueDust100(vl => [...vl, sv]);
+          setReceiveTimeDust100(rt => [...rt, st]);
+        }), 2000);
+      return () => {clearInterval(loop)};
+    }
+    
+  },[isStartDust100]);
 
-  const options = {
+  const optionsDust100 = {
       responsive: false,
       plugins: {
           legend: {
@@ -62,19 +65,138 @@ function App() {
           }
       }
   };
-  const labels = [receiveTime];
-  const data = {
-      labels,
+  const dataDust100 = {
+      labels: receiveTimeDust100,
       datasets: [
           {
               label: 'Dust100',
-              data: [sensorValue],
+              data: sensorValueDust100,
               borderColor: 'rgb(255, 99,132)',
               backgroundColor: 'rgba(255, 99, 132, 0.5)',
+              borderWidth: 1,
+              pointRadius: 1,
           }
       ],
   };
 
+
+
+const mountedDust25 = useRef(false);
+const [sensorValueDust25, setSensorValueDust25] = useState<number[]>([]);
+const [receiveTimeDust25, setReceiveTimeDust25] = useState<string[]>([]);
+
+const [isStartDust25, setIsStartDust25] = useState(true);
+
+function toggleStartDust25(){
+  setIsStartDust25(!isStartDust25);
+  mountedDust25.current=isStartDust25
+}
+
+useEffect(()=> {
+  if(!mountedDust25.current) {
+    mountedDust25.current = true;
+  }
+  else {
+    const loop = setInterval(
+      () => axios.get(`http://43.201.154.68:8100/Get_Value?sensor_type=DUST25`)
+      .then((response) => {
+        console.log("res");
+        const da = response.data['data'][0];
+        let sv = Number(da.sensor_value);
+        let st = da.receive_time;
+        setSensorValueDust25(vl => [...vl, sv]);
+        setReceiveTimeDust25(rt => [...rt, st]);
+      }), 5000);
+    return () => {clearInterval(loop)};
+  }
+  
+},[isStartDust25]);
+
+const optionsDust25 = {
+    responsive: false,
+    plugins: {
+        legend: {
+            display: false,
+            position: 'top' as const,
+        },
+        title: {
+            display: true,
+            text: 'chartDust25',
+        }
+    }
+};
+const dataDust25 = {
+    labels: receiveTimeDust25,
+    datasets: [
+        {
+            label: 'Dust25',
+            data: sensorValueDust25,
+            borderColor: 'rgb(255, 99,132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            borderWidth: 1,
+            pointRadius: 1,
+        }
+    ],
+};
+
+
+  const mountedTemp = useRef(false);
+  const [sensorValueTemp, setSensorValueTemp] = useState<number[]>([]);
+  const [receiveTimeTemp, setReceiveTimeTemp] = useState<string[]>([]);
+
+  const [isStartTemp, setIsStartTemp] = useState(true);
+
+  function toggleStartTemp(){
+    setIsStartTemp(!isStartTemp);
+    mountedTemp.current=isStartTemp
+  }
+
+  useEffect(()=> {
+    if(!mountedTemp.current) {
+      mountedTemp.current = true;
+    }
+    else {
+      const loop = setInterval(
+        () => axios.get(`http://43.201.154.68:8100/Get_Value?sensor_type=TEMP`)
+        .then((response) => {
+          console.log("res");
+          const da = response.data['data'][0];
+          let sv = Number(da.sensor_value);
+          let st = da.receive_time;
+          setSensorValueTemp(vl => [...vl, sv]);
+          setReceiveTimeTemp(rt => [...rt, st]);
+        }), 5000);
+      return () => {clearInterval(loop)};
+    }
+    
+  },[isStartTemp]);
+
+  const optionsTemp = {
+      responsive: false,
+      plugins: {
+          legend: {
+              display: false,
+              position: 'top' as const,
+          },
+          title: {
+              display: true,
+              text: 'chartTemp',
+          }
+      }
+  };
+  const dataTemp = {
+      labels: receiveTimeTemp,
+      datasets: [
+          {
+              label: 'Temp',
+              data: sensorValueTemp,
+              borderColor: 'rgb(255, 99,132)',
+              backgroundColor: 'rgba(255, 99, 132, 0.5)',
+              borderWidth: 1,
+              pointRadius: 1,
+          }
+      ],
+  };
 
   return (
     <div className="App">
@@ -85,13 +207,32 @@ function App() {
       </header>
       <body className="App-body">
         <Grid container>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={4}>
             <Button
-              // onClick={SI}
+              // sx={{ mr:0, alignContent: 'flex-end' }}
+              onClick={toggleStartDust100}
             >
-              Start
+              자료수집 {isStartDust100? "시작" : "멈춤"}
             </Button>
-            <Line options={options} data={data} style={{position: "relative", height: "200px"}} />
+            <Line options={optionsDust100} data={dataDust100} style={{position: "relative", width: "500px"}} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Button
+              // sx={{ mr:0, alignContent: 'flex-end' }}
+              onClick={toggleStartDust25}
+            >
+              자료수집 {isStartDust25? "시작" : "멈춤"}
+            </Button>
+            <Line options={optionsDust25} data={dataDust25} style={{position: "relative", width: "500px"}} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Button
+              // sx={{ mr:0, alignContent: 'flex-end' }}
+              onClick={toggleStartTemp}
+            >
+              자료수집 {isStartTemp? "시작" : "멈춤"}
+            </Button>
+            <Line options={optionsTemp} data={dataTemp} style={{position: "relative", width: "500px"}} />
           </Grid>
         </Grid>
         <Card
---
+++ src/component/ChartHumi.tsx
@@ -0,0 +1,37 @@
+import { Line } from 'react-chartjs-2';
+
+const HumiChart = (props: any) => {
+    var sensorValue = props.sensor_value;
+    var receiveTime = props.receive_time;
+
+    const options = {
+      responsive: false,
+      plugins: {
+          legend: {
+              display: false,
+              position: 'top' as const,
+          },
+          title: {
+              display: true,
+              text: 'Humidity',
+          }
+      }
+    };
+    const data = {
+        labels: receiveTime,
+        datasets: [
+            {
+                label: 'Humidity',
+                data: sensorValue,
+                borderColor: 'rgb(255, 99,132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                borderWidth: 1,
+                pointRadius: 1,
+            }
+        ],
+    };
+
+    return <Line options={options} data={data} style={{position: "relative", height: "200px"}} />
+}
+
+export default HumiChart;
