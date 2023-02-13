e045c8c45a020a74fa6ca5004866b70b258bd446
windyimp81 <windyimp@spesys.co.kr>
2023-02-13 14:03:46 +0900

    update api

--- src/App2.tsx
+++ src/App2.tsx
@@ -1,27 +1,9 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import './App.css';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement,
+  LineElement, Title, Tooltip, Legend } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import {
-  Card,
-  useTheme,
-  styled,
-  Box,
-  Grid
-} from '@mui/material';
-import { apiRequest } from './utils/apiRequest';
-import type { RespResult } from './models/responsetype';
-import useRefMounted from './hooks/useRefMounted';
-import { Device1 } from './models/device1';
+import { Card, useTheme, styled, Box, Grid, Button } from '@mui/material';
 import axios from 'axios';
 
 ChartJS.register(
@@ -34,73 +16,65 @@ ChartJS.register(
   Legend
 );
 
-interface data1 {
-  device_group : string,
-  device_id : string,
-  device_time : string,
-  id : string,
-  receive_time : string,
-  sensor_type : string,
-  sensor_value : string,
-  time_string : string
-}
-
-
 function App() {
 
-  const options = {
-    responsive: false,
-    plugins: {
-      legend: {
-        display: false,
-        position: 'top' as const,
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart',
-      },
-    },
-  };
-  
-  // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  
-  // const data = {
-  //   labels,
-  //   datasets: [
-  //     {
-  //       label: 'Dataset 1',
-  //       // data: labels.map(() => Number()),
-  //       data: [30, 50, 20, 60, 100,70],
-  //       borderColor: 'rgb(255, 99, 132)',
-  //       backgroundColor: 'rgba(255, 99, 132, 0.5)',
-  //     },
-  //     {
-  //       label: 'Dataset 2',
-  //       data: [20, 40, 70, 30, 150, 100],
-  //       borderColor: 'rgb(53, 162, 235)',
-  //       backgroundColor: 'rgba(53, 162, 235, 0.5)',
-  //     },
-  //   ],
+  const [data1, setData1] = useState({
+    sensor_value: 0,
+    receive_time: ''
+  });
+
+  useEffect(() => {
+    setInterval(
+      () => axios.get(`http://43.201.154.68:8100/Get_Value?sensor_type=DUST100`)
+      .then((response) => {
+        setData1(response.data['data'][0]);
+      }), 2000
+    )
+  }, []);
+  const [sensorValue, setSensorValue] = useState(0);
+  const [receiveTime, setReceiveTime] = useState('');
+  // const fetchDatas = async() => {
+  //     const response = await axios.get('http://43.201.154.68:8100/Get_Value?sensor_type=DUST100');
+  //     setData1(response.data['data'][0]);
   // };
 
-  const [data, setData] = useState<data1>();
-  const [dataLists, setDataLists] = useState<data1[]>([]);
-  // const [activeIndex, setActiveIndex] = useState(0);
-  // const [activeItem, setActiveItem] = useState(0);
-  const fetchDatas = async() => {
-    const response = await axios.get('http://43.201.154.68:8100/Get_Value?sensor_type=DUST100');
-    setData(response.data['data']);
-    // setActiveItem(response.data[0]);
+  // useEffect(() => {
+  //     fetchDatas();
+  // }, []);
+
+  useEffect(() => {
+      let sv = Number(data1?.sensor_value);
+      let st = data1.receive_time;
+      setSensorValue(sv);
+      setReceiveTime(st);
+  }, [data1])
+
+  const options = {
+      responsive: false,
+      plugins: {
+          legend: {
+              display: false,
+              position: 'top' as const,
+          },
+          title: {
+              display: true,
+              text: 'chartDust100',
+          }
+      }
+  };
+  const labels = [receiveTime];
+  const data = {
+      labels,
+      datasets: [
+          {
+              label: 'Dust100',
+              data: [sensorValue],
+              borderColor: 'rgb(255, 99,132)',
+              backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          }
+      ],
   };
 
-  useEffect(() => {
-    fetchDatas();
-  }, []);
-
-  useEffect(() => {
-    // setDataLists(el => [...el, (sensor_type: '', sensor_value: '', time_string: '')]);
-    console.log('data:', data?.sensor_value);
-  }, [data])
 
   return (
     <div className="App">
@@ -111,15 +85,14 @@ function App() {
       </header>
       <body className="App-body">
         <Grid container>
-          {/* <Grid item xs={12} sm={6}>
-            <Line options={options} data={data.map(() => Number())} style={{position: "relative", height: "200px"}}/>
-          </Grid> */}
-          {/* <Grid item xs={12} sm={6}>
-            <Line options={options} data={data} style={{position: "relative", height: "200px"}}/>
-          </Grid>
           <Grid item xs={12} sm={6}>
-            <Line options={options} data={data} style={{position: "relative", height: "200px"}}/>
-          </Grid> */}
+            <Button
+              // onClick={SI}
+            >
+              Start
+            </Button>
+            <Line options={options} data={data} style={{position: "relative", height: "200px"}} />
+          </Grid>
         </Grid>
         <Card
           variant="outlined"
